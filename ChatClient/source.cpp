30e5eb35ed7fcd2120dd36b745a8a4e069f18746
Oldbobik <victorishchyshyn@gmail.com>
2024-12-12 01:21:08 +0200

    Track large_file.dat with Git LFS

---
+++ ChatClient/source.cpp
@@ -0,0 +1,230 @@
+#define NOMINMAX
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+std::string logFile = "ClientLog.txt";
+#define PIPE_NAME R"(\\.\pipe\ChatPipe)"
+
+std::string getCurrentTime() {
+    time_t now = time(0);
+    tm* localTime = localtime(&now);
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", localTime);
+    return std::string(buffer);
+}
+void saveToLog(const std::string& fileName, const std::string& message) {
+    std::ofstream logFile(fileName, std::ios::app);
+    if (logFile.is_open()) {
+        logFile << getCurrentTime() << " " << message << "\n";
+        logFile.close();
+    }
+    else {
+        std::cerr << "Error: Unable to open log file " << fileName << "\n";
+    }
+}
+//----------------------------------------------------------
+void startNamedPipesChat()
+{
+    std::ofstream file(logFile, std::ios::app);
+    file << "\n";
+    HANDLE hPipe;
+    char buffer[1024];
+    DWORD bytesRead;
+
+    std::cout << "Waiting for server to start..";
+    saveToLog(logFile, "Waiting for server to start...");
+
+    while (true) {
+        if (WaitNamedPipeA(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
+            break;
+        }
+        std::cout << ".";
+        Sleep(1000);
+    }
+
+    std::cout << "\nConnecting to the server...\n";
+    saveToLog(logFile, "Connecting to the server...");
+
+    hPipe = CreateFileA(
+        PIPE_NAME,
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        nullptr,
+        OPEN_EXISTING,
+        0,
+        nullptr);
+
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        std::cerr << "Failed to connect to the server. Error code: " << GetLastError() << "\n";
+        saveToLog(logFile, "Failed to connect to the server. Error code: " + std::to_string(GetLastError()));
+        return;
+    }
+
+    std::cout << "Connected to the server via Named Pipes!\n";
+    saveToLog(logFile, "Connected to the server via Named Pipes!");
+    char clientMessage[1024];
+    DWORD bytesWritten;
+    while (true) {
+        std::cout << "You: ";
+        std::cin.getline(clientMessage, sizeof(clientMessage));
+        saveToLog(logFile, "You: " + std::string(clientMessage));
+        if (strcmp(clientMessage, "/menu") == 0) {
+            system("cls");
+            WriteFile(hPipe, "/menu", 6, &bytesWritten, nullptr);
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            CloseHandle(hPipe);
+            return;
+        }
+        if (strcmp(clientMessage, "/exit") == 0) {
+            std::cout << "The chat session has been ended.\n";
+            WriteFile(hPipe, "/exit", 6, &bytesWritten, nullptr);
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            CloseHandle(hPipe);
+            exit(0);
+        }
+        if (!WriteFile(hPipe, clientMessage, strlen(clientMessage), &bytesWritten, nullptr)) {
+            std::cerr << "Error sending data. Error code: " << GetLastError() << "\n";
+            saveToLog(logFile, "Error sending data. Error code: " + std::to_string(GetLastError()));
+            break;
+        }
+
+        if (!ReadFile(hPipe, buffer, sizeof(buffer) - 1, &bytesRead, nullptr)) {
+            std::cerr << "Error reading data. Error code: " << GetLastError() << "\n";
+            saveToLog(logFile, "Error reading data. Error code: " + std::to_string(GetLastError()));
+            break;
+        }
+        buffer[bytesRead] = '\0';
+
+        std::cout << "Server: " << buffer << "\n";
+        saveToLog(logFile, "Server: " + std::string(buffer));
+        if (strcmp(buffer, "/menu") == 0) {
+            system("cls");
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            CloseHandle(hPipe);
+            return;
+        }
+        if (strcmp(buffer, "/exit") == 0) {
+            std::cout << "The chat session has been ended by the server.\n";
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            CloseHandle(hPipe);
+            exit(0);
+        }
+    }
+    CloseHandle(hPipe);
+    exit(0);
+}
+//----------------------------------------------------------
+void startFileMappingChat() {
+    std::ofstream file(logFile, std::ios::app);
+    file << "\n";
+    const char* fileMappingName = "Local//ChatFile";
+    const int bufferSize = 1024;
+    char* sharedMemory;
+    HANDLE hMapFile;
+
+    std::cout << "Waiting for server to start..";
+    saveToLog(logFile, "Waiting for server to start...");
+
+    while (true) {
+        hMapFile = OpenFileMappingA(
+            FILE_MAP_ALL_ACCESS,         
+            FALSE,                  
+            fileMappingName);          
+        if (hMapFile != NULL) {
+            break;
+        }
+        std::cout << ".";
+        Sleep(1000);
+    }
+    sharedMemory = (char*)MapViewOfFile(hMapFile, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, bufferSize);
+    if (sharedMemory == NULL) {
+        std::cerr << "Could not map view of file. Error code: " << GetLastError() << "\n";
+        saveToLog(logFile, "Failed to map view of file. Error code: " + std::to_string(GetLastError()));
+        CloseHandle(hMapFile);
+        return;
+    }
+    sharedMemory[0] = '1';
+    std::cout << "\nConnected to the server via File Mapping!\n";
+    saveToLog(logFile, "Connected to the server via File Mapping!");
+    char clientMessage[1024];
+    while (true) {
+        std::cout << "You: ";
+        std::cin.getline(clientMessage, sizeof(clientMessage));
+        saveToLog(logFile, "You: " + std::string(clientMessage));
+
+        strcpy(sharedMemory + 1, clientMessage); 
+        sharedMemory[0] = '0';                  
+        if (strcmp(clientMessage, "/menu") == 0) {
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            system("cls");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            break;
+        }
+        if (strcmp(clientMessage, "/exit") == 0) {
+            std::cout << "The chat session has been ended.\n";
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            exit(0);
+        }
+
+        while (sharedMemory[0] == '0') {
+            Sleep(100);
+        }
+
+        std::cout << "Server: " << (sharedMemory + 1) << "\n";
+        saveToLog(logFile, "Server: " + std::string(sharedMemory + 1));
+        if (strcmp(sharedMemory + 1, "/menu") == 0) {
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            system("cls");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            break;
+        }
+        if (strcmp(sharedMemory + 1, "/exit") == 0) {
+            std::cout << "The chat session has been ended by the server.\n";
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            exit(0);
+        }
+        sharedMemory[0] = '1';
+    }
+    UnmapViewOfFile(sharedMemory);
+    CloseHandle(hMapFile);
+}
+//----------------------------------------------------------
+
+int main() {
+    while (true) {
+        int choice;
+        std::cout << "Choose communication method:\n";
+        std::cout << "1. Named Pipes\n";
+        std::cout << "2. File Mapping\n";
+        std::cout << "3. Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch (choice) {
+        case 1:
+            startNamedPipesChat();
+            break;
+        case 2:
+            startFileMappingChat();
+            break;
+        case 3:
+            std::cout << "Exiting the program.\n";
+            return 0;
+        default:
+            std::cout << "Invalid choice. Try again.\n";
+            break;
+        }
+    }
+    return 0;
+}
---
+++ ChatServer/source.cpp
@@ -0,0 +1,245 @@
+#define NOMINMAX
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+std::string logFile = "ServerLog.txt";
+#define PIPE_NAME R"(\\.\pipe\ChatPipe)"
+
+
+std::string getCurrentTime() {
+    time_t now = time(0);
+    tm* localTime = localtime(&now);
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", localTime);
+    return std::string(buffer);
+}
+void saveToLog(const std::string& fileName, const std::string& message) {
+    std::ofstream logFile(fileName, std::ios::app);
+    if (logFile.is_open()) {
+        logFile << getCurrentTime() << " " << message << "\n";
+        logFile.close();
+    }
+    else {
+        std::cerr << "Error: Unable to open log file " << fileName << "\n";
+    }
+}
+//---------------------------------------------------------
+void startNamedPipesChat(){
+    std::ofstream file(logFile, std::ios::app);
+    file << "\n";
+    HANDLE hPipe;
+    char buffer[1024];
+    DWORD bytesRead;
+
+    std::cout << "Chat server started. Waiting for client...\n";
+    saveToLog(logFile, "Chat server started. Waiting for client...");
+
+    hPipe = CreateNamedPipeA(
+        PIPE_NAME,
+        PIPE_ACCESS_DUPLEX,
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+        1,
+        1024,
+        1024,
+        0,
+        nullptr);
+
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        std::cerr << "Failed to create named pipe. Error code: " << GetLastError() << "\n";
+        saveToLog(logFile, "Failed to create named pipe. Error code: " + std::to_string(GetLastError()));
+        return;
+    }
+
+    if (!ConnectNamedPipe(hPipe, nullptr)) {
+        std::cerr << "Failed to connect client. Error code: " << GetLastError() << "\n";
+        saveToLog(logFile, "Failed to connect client. Error code: " + std::to_string(GetLastError()));
+        CloseHandle(hPipe);
+        return;
+    }
+
+    std::cout << "Client connected via Named Pipes!\n";
+    saveToLog(logFile, "Client connected via Named Pipes!");
+    char serverMessage[1024];
+    DWORD bytesWritten;
+    while (true) {
+        if (!ReadFile(hPipe, buffer, sizeof(buffer) - 1, &bytesRead, nullptr)) {
+            std::cerr << "Error reading data. Error code: " << GetLastError() << "\n";
+            saveToLog(logFile, "Error reading data. Error code: " + std::to_string(GetLastError()));
+            break;
+        }
+        buffer[bytesRead] = '\0';
+
+        std::cout << "Client: " << buffer << "\n";
+        saveToLog(logFile, "Client: " + std::string(buffer));
+        if (strcmp(buffer, "/menu") == 0) {
+            system("cls");
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            DisconnectNamedPipe(hPipe);
+            CloseHandle(hPipe);
+            return;
+        }
+        if (strcmp(buffer, "/exit") == 0) {
+            std::cout << "The chat session has been ended by the client.\n";
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            DisconnectNamedPipe(hPipe);
+            CloseHandle(hPipe);
+            exit(0);
+        }
+
+        std::cout << "You: ";
+        std::cin.getline(serverMessage, sizeof(serverMessage));
+        saveToLog(logFile, "You: " + std::string(serverMessage));
+        if (strcmp(serverMessage, "/menu") == 0) {
+            system("cls");
+            WriteFile(hPipe, "/menu", 6, &bytesWritten, nullptr);
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            DisconnectNamedPipe(hPipe);
+            CloseHandle(hPipe);
+            return;  
+        }
+        if (strcmp(serverMessage, "/exit") == 0) {
+            std::cout << "The chat session has been ended.\n";
+            WriteFile(hPipe, "/exit", 6, &bytesWritten, nullptr);
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            DisconnectNamedPipe(hPipe);
+            CloseHandle(hPipe);
+            exit(0);
+        }
+        if (!WriteFile(hPipe, serverMessage, strlen(serverMessage), &bytesWritten, nullptr)) {
+            std::cerr << "Error sending data. Error code: " << GetLastError() << "\n";
+            saveToLog(logFile, "Error sending data. Error code: " + std::to_string(GetLastError()));
+            break;
+        }
+    }
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+    exit(0);
+}
+//---------------------------------------------------------
+void startFileMappingChat() {
+    std::ofstream file(logFile, std::ios::app);
+    file << "\n";
+    int bufferSize = 1024;
+    const char* fileMappingName = "Local//ChatFile";
+    char* sharedMemory;
+    HANDLE hMapFile;
+
+    std::cout << "Chat server started. Waiting for client...\n";
+    saveToLog(logFile, "Chat server started. Waiting for client...");
+
+    hMapFile = CreateFileMappingA(
+        INVALID_HANDLE_VALUE,    
+        NULL,                   
+        PAGE_READWRITE,         
+        0,                       
+        bufferSize,             
+        fileMappingName);          
+
+    if (hMapFile == NULL) {
+        std::cerr << "Could not create file mapping object. Error code: " << GetLastError() << "\n";
+        saveToLog(logFile, "Failed to create file mapping object. Error code: " + std::to_string(GetLastError()));
+        return;
+    }
+
+    sharedMemory = (char*)MapViewOfFile(hMapFile, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, bufferSize);
+    if (sharedMemory == NULL) {
+        std::cerr << "Could not map view of file. Error code: " << GetLastError() << "\n";
+        saveToLog(logFile, "Failed to map view of file. Error code: " + std::to_string(GetLastError()));
+        CloseHandle(hMapFile);
+        return;
+    }
+    while (true) {
+        if (sharedMemory[0] == '1') {
+            std::cout << "Client connected via File Mapping!\n";
+            saveToLog(logFile, "Client connected via File Mapping!");
+            break;
+        }
+    }
+    char serverMessage[1024];
+    DWORD bytesWritten;
+    while (true) {
+        while (sharedMemory[0] == '1') {
+            Sleep(100);
+        }
+
+        std::cout << "Client: " << (sharedMemory + 1) << "\n";
+        saveToLog(logFile, "Client: " + std::string(sharedMemory + 1));
+
+        if (strcmp(sharedMemory + 1, "/menu") == 0) {
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            system("cls");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            break;
+        }
+        if (strcmp(sharedMemory + 1, "/exit") == 0) {
+            std::cout << "The chat session has been ended by the client.\n";
+            saveToLog(logFile, "The chat session has been ended by the client.");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            exit(0);
+        }
+
+        std::cout << "You: ";
+        std::cin.getline(serverMessage, sizeof(serverMessage));
+        saveToLog(logFile, "You: " + std::string(serverMessage));
+
+        strcpy(sharedMemory + 1, serverMessage); 
+        sharedMemory[0] = '1';                  
+
+        if (strcmp(serverMessage, "/menu") == 0) {
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            system("cls");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            break;
+        }
+        if (strcmp(serverMessage, "/exit") == 0) {
+            std::cout << "The chat session has been ended.\n";
+            saveToLog(logFile, "The chat session has been ended by the server.");
+            UnmapViewOfFile(sharedMemory);
+            CloseHandle(hMapFile);
+            exit(0);
+        }
+        while (sharedMemory[0] != '1') {
+            Sleep(100);
+        }
+    }
+    UnmapViewOfFile(sharedMemory);
+    CloseHandle(hMapFile);
+}
+//---------------------------------------------------------
+
+int main() {
+    while (true) {
+        int choice;
+        std::cout << "Choose communication method:\n";
+        std::cout << "1. Named Pipes\n";
+        std::cout << "2. File Mapping\n";
+        std::cout << "3. Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch (choice) {
+        case 1:
+            startNamedPipesChat();
+            break;
+        case 2:
+            startFileMappingChat();
+            break;
+        case 3:
+            std::cout << "Exiting the program.\n";
+            return 0;
+        default:
+            system("cls");
+            std::cout << "Invalid choice. Try again.\n";
+            break;
+        }
+    }
+    return 0;
+}
